c637213e6511b592b1412d69895a5de72f6490a9
dmtG <dimitri.gevers@gmail.com>
2020-10-08 12:25:44 +0200

    rabbitMQ final

--- src/main/java/be/vdab/catalogus/repositories/ArtikelRepository.java
+++ src/main/java/be/vdab/catalogus/repositories/ArtikelRepository.java
@@ -3,6 +3,6 @@ package be.vdab.catalogus.repositories;
 import be.vdab.catalogus.domain.Artikel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArtikelRepository extends JpaRepository<Artikel,Long> {
+public interface ArtikelRepository extends JpaRepository<Artikel, Long> {
 
 }
--- src/main/java/be/vdab/catalogus/services/ArtikelService.java
+++ src/main/java/be/vdab/catalogus/services/ArtikelService.java
@@ -4,6 +4,6 @@ import be.vdab.catalogus.domain.Artikel;
 
 public interface ArtikelService {
 
-    void create(Artikel artikel);
+    public void create(Artikel artikel);
 
 }
